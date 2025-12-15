5cefc50d0f445257f998bb323b0563041e8944f5
eipmavinna <146777395+eipmavinna@users.noreply.github.com>
2025-12-15 16:37:39 -0500

    changed some things yea

--- app/static/scripts/searchScript.js
+++ app/static/scripts/searchScript.js
@@ -8,6 +8,14 @@ document.addEventListener("DOMContentLoaded", async () => {
         await getFilteredPets();
     });
     const submitBtn = document.getElementById("submit-btn");
+    const favButton = document.getElementById("favorite-button");
+    if (IsLoggedInSearch()) {
+        console.log("logged in");
+        favButton.addEventListener("click", () => addToFavoritesSearch(favButton.dataset.petId));
+    }
+    else {
+        favButton.remove();
+    }
 });
 async function getFilteredPets() {
     const form = document.getElementById("filterForm");
@@ -75,7 +83,7 @@ async function loadHomePetsSearch(id, newDiv) {
     const organizations = await validateHomeJSONSearch(response2);
     const orgLocationCity = organizations.data[0].attributes.citystate;
     if (imageURL == null) {
-        btn.innerHTML = `<img src="static/icons/petStubImage.png" alt="No stub Available"><p>${name}</p><p>${orgLocationCity}</p>`;
+        btn.innerHTML = `<img src="/static/icons/petStubImage.png" alt="No stub Available"><p>${name}</p><p>${orgLocationCity}</p>`;
     }
     else {
         btn.innerHTML = `<img src="${imageURL}" alt="No Image Available"><p>${name}</p><p>${orgLocationCity}</p>`;
@@ -186,7 +194,7 @@ async function fillModalSearch(id) {
     }
     const img = document.getElementById("petImage");
     if (imageURL == null) {
-        img.src = 'static/icons/petStubImage.png';
+        img.src = '/static/icons/petStubImage.png';
         img.alt = 'No stub Available';
         if (modalBodyDiv) {
             modalBodyDiv.append(img);
